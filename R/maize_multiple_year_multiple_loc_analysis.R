3cfb8ac853a970f66b8a73ce085508f03aef18cd
dengfei2013 <dengfei_2013@163.com>
2019-04-22 14:45:13 +0800

    aaa

--- R/maize_multiple_year_multiple_loc_analysis.R
+++ R/maize_multiple_year_multiple_loc_analysis.R
@@ -13,6 +13,8 @@
 #' dat = dd
 #' head(dat)
 #' str(dat)
+#' maize_multiple_year_multiple_loc_analysis(dat,json=F)
+#' maize_multiple_year_multiple_loc_analysis(dat)
 
 
 
@@ -54,6 +56,19 @@ maize_multiple_year_multiple_loc_analysis = function(dat,json=TRUE){
 
   loc_lsd_value0.05 = LSD_value(mod,"Loc",alpha = 0.05);loc_lsd_value0.01 = LSD_value(mod,"Loc",alpha = 0.01)
 
+
+  year_lsd0.05 = LSD_test(mod,"Year",alpha=0.05)$groups
+  year_lsd0.05_Name = rownames(year_lsd0.05)
+  year_lsd0.05$Name = rownames(year_lsd0.05)
+  year_lsd0.05 = year_lsd0.05[,c(3,1,2)]
+
+  year_lsd0.01 = LSD_test(mod,"Year",alpha=0.01)$groups
+  year_lsd0.01_Name = rownames(year_lsd0.01)
+  year_lsd0.01$Name = rownames(year_lsd0.01)
+  year_lsd0.01 = year_lsd0.01[,c(3,1,2)]
+
+  year_lsd_value0.05 = LSD_value(mod,"Year",alpha = 0.05);year_lsd_value0.01 = LSD_value(mod,"Year",alpha = 0.01)
+
   loc.cul_lsd0.05 = LSD_test(mod,"Loc.Cul",alpha = 0.05)$groups
   loc.cul_lsd0.05_Name = rownames(loc.cul_lsd0.05)
   loc.cul_lsd0.05$Name = rownames(loc.cul_lsd0.05)
@@ -66,9 +81,23 @@ maize_multiple_year_multiple_loc_analysis = function(dat,json=TRUE){
 
   loc.cul_lsd_value0.05 = LSD_value(mod,"Loc.Cul",alpha = 0.05);loc.cul_lsd_value0.01 = LSD_value(mod,"Loc.Cul",alpha = 0.01)
 
+  year.cul_lsd0.05 = LSD_test(mod,"Year.Cul",alpha = 0.05)$groups
+  year.cul_lsd0.05_Name = rownames(year.cul_lsd0.05)
+  year.cul_lsd0.05$Name = rownames(year.cul_lsd0.05)
+  year.cul_lsd0.05 = year.cul_lsd0.05[,c(3,1,2)]
+
+  year.cul_lsd0.01 = LSD_test(mod,"Year.Cul",alpha = 0.01)$groups
+  year.cul_lsd0.01_Name = rownames(year.cul_lsd0.01)
+  year.cul_lsd0.01$Name = rownames(year.cul_lsd0.01)
+  year.cul_lsd0.01 = year.cul_lsd0.01[,c(3,1,2)]
+
+  year.cul_lsd_value0.05 = LSD_value(mod,"Year.Cul",alpha = 0.05);year.cul_lsd_value0.01 = LSD_value(mod,"Year.Cul",alpha = 0.01)
+
   re = c(aa,cul_lsd0.05,cul_lsd0.01,cul_lsd_value0.05,cul_lsd_value0.01,
          loc_lsd0.05,loc_lsd0.01,loc_lsd_value0.05,loc_lsd_value0.01,
          loc.cul_lsd0.05,loc.cul_lsd0.01,loc.cul_lsd_value0.05,loc.cul_lsd_value0.01,
+         year_lsd0.05,year_lsd0.01,year_lsd_value0.05,year_lsd_value0.01,
+         year.cul_lsd0.05,year.cul_lsd0.01,year.cul_lsd_value0.05,year.cul_lsd_value0.01,
          cv)
 
   if(json){
@@ -77,6 +106,8 @@ maize_multiple_year_multiple_loc_analysis = function(dat,json=TRUE){
     re = list(aa,cul_lsd0.05,cul_lsd0.01,cul_lsd_value0.05,cul_lsd_value0.01,
               loc_lsd0.05,loc_lsd0.01,loc_lsd_value0.05,loc_lsd_value0.01,
               loc.cul_lsd0.05,loc.cul_lsd0.01,loc.cul_lsd_value0.05,loc.cul_lsd_value0.01,
+              year_lsd0.05,year_lsd0.01,year_lsd_value0.05,year_lsd_value0.01,
+              year.cul_lsd0.05,year.cul_lsd0.01,year.cul_lsd_value0.05,year.cul_lsd_value0.01,
               cv)
   }
   return(re)
